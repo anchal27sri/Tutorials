d5c90169f7f1024e57211240fbcc9046b8a19104
Anchal Srivastava <srivastavaanchal000@gmail.com>
2020-09-27 00:01:26 +0530

    Adrian ComputerVision Tutorial Part1

---
+++ ComputerVision/Adrian/binog.py
@@ -0,0 +1,2 @@
+from tensorflow.python.keras.utils import data_utils
+weights_path = data_utils.get_file(fname='shakal',origin='https://storage.googleapis.com/tensorflow/keras-applications/resnet/resnet50_weights_tf_dim_ordering_tf_kernels.h5')
