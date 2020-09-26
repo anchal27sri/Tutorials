6bf3607640214cccdb959f0087008ea9256ebd04
Anchal Srivastava <srivastavaanchal000@gmail.com>
2020-09-27 00:23:01 +0530

    temp

--- ComputerVision/Adrian/binog.py
+++
@@ -1,2 +0,0 @@
-from tensorflow.python.keras.utils import data_utils
-weights_path = data_utils.get_file(fname='shakal',origin='https://storage.googleapis.com/tensorflow/keras-applications/resnet/resnet50_weights_tf_dim_ordering_tf_kernels.h5')
